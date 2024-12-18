01f91579a719448c1c48518b451513ab56770b14
Asmodeus-xs <prisrak23@gmail.com>
2024-12-18 15:29:44 +0200

    changed meal image link

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -42,7 +42,7 @@ export default function MealsDetailsPage({ params }) {
         <>
             <header className={classes.header}>
                 <div className={classes.image}>
-                    <Image src={`/next-app/${meal.image}`} alt='meal image' fill />
+                    <Image src={`/${meal.image}`} alt='meal image' fill />
                 </div>
                 <div className={classes.headerText}>
                     <h1>{meal.title}</h1>
